4fafa3578c71feb72821f4e473e8b38e8c288949
lubniewskif <lubniewskif@ryanair.com>
2020-03-22 18:38:41 +0100

    move common provider functions for DI to single package

--- cmd/create/main.go
+++ cmd/create/main.go
@@ -5,48 +5,25 @@ import (
 	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/aws/external"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/projects/secure-notes/internal/creating"
 	"github.com/projects/secure-notes/internal/http/rest"
+	"github.com/projects/secure-notes/internal/platform/provider"
 	"github.com/projects/secure-notes/internal/platform/security"
 	"github.com/projects/secure-notes/internal/platform/web"
-	db "github.com/projects/secure-notes/internal/storage/dynamodb"
-	"go.uber.org/zap"
 )
 
 var createNoteHandler web.Handler
 
 func init() {
-	// load AWS config
-	cfg, err := external.LoadDefaultAWSConfig()
-	if err != nil {
-		panic("cannot read AWS configuration")
-	}
+	cfg := provider.AWSConfig()
+	storage := provider.DynamoStorage(cfg, os.Getenv("NOTES_TABLE"))
 
-	// setup DynamoDB adapter
-	dbCli := dynamodb.New(cfg)
-	notesTableName := os.Getenv("NOTES_TABLE")
-	storage := db.NewStorage(dbCli, notesTableName)
-
-	// setup domain service
 	now := func() time.Time { return time.Now().UTC() }
 	hashGen := security.GenerateHashWithSalt
 	creator := creating.NewService(storage, now, hashGen)
 
-	// setup API Gateway adapter
 	handler := rest.CreateNote(creator)
-
-	// setup web middleware
-	logger, err := zap.NewProductionConfig().Build()
-	if err != nil {
-		panic("cannot initialize logger")
-	}
-	middleware := &web.Middleware{
-		Logger: logger.Sugar(),
-	}
-
-	// setup handler wrapped with middleware
+	middleware := provider.Middleware()
 	createNoteHandler = middleware.WrapWithCorsAndLogging(handler)
 }
 
--- cmd/get/main.go
+++ cmd/get/main.go
@@ -4,45 +4,20 @@ import (
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go-v2/aws/external"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/projects/secure-notes/internal/getting"
 	"github.com/projects/secure-notes/internal/http/rest"
+	"github.com/projects/secure-notes/internal/platform/provider"
 	"github.com/projects/secure-notes/internal/platform/web"
-	db "github.com/projects/secure-notes/internal/storage/dynamodb"
-	"go.uber.org/zap"
 )
 
 var getNoteHandler web.Handler
 
 func init() {
-	// load AWS config
-	cfg, err := external.LoadDefaultAWSConfig()
-	if err != nil {
-		panic("cannot read AWS configuration")
-	}
-
-	// setup DynamoDB adapter
-	dbCli := dynamodb.New(cfg)
-	notesTableName := os.Getenv("NOTES_TABLE")
-	storage := db.NewStorage(dbCli, notesTableName)
-
-	// setup domain service
+	cfg := provider.AWSConfig()
+	storage := provider.DynamoStorage(cfg, os.Getenv("NOTES_TABLE"))
 	getter := getting.NewService(storage)
-
-	// setup API Gateway adapter
 	handler := rest.GetNote(getter)
-
-	// setup web middleware
-	logger, err := zap.NewProductionConfig().Build()
-	if err != nil {
-		panic("cannot initialize logger")
-	}
-	middleware := &web.Middleware{
-		Logger: logger.Sugar(),
-	}
-
-	// setup handler wrapped with middleware
+	middleware := provider.Middleware()
 	getNoteHandler = middleware.WrapWithCorsAndLogging(handler)
 }
 
